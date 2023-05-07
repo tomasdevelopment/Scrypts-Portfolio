8c763e966bc45190565c016748afcf724552f664
TomasSuarez <83978153+tomasdevelopment@users.noreply.github.com>
2023-05-07 18:17:35 -0500

    Update rectanglearea.java
    SimpleJavaScrypt

--- rectanglearea.java
+++ rectanglearea.java
@@ -1,5 +1,22 @@
-package packetedoritos;
 
+
+    // Method to calculate the area of a rectangle
+ 
 public class rectanglearea {
+	
+	    // Method to calculate the area of a rectangle
+	    public static int calculateArea(int length, int width) {
+	        return length * width;
+	    }
 
-}
+	    public static void main(String[] args) {
+	        // Example rectangle dimensions
+	        int length = 10;
+	        int width = 5;
+
+	        // Calculate and display the area of the rectangle
+	        int area = calculateArea(length, width);
+	        System.out.println("The area of the rectangle with length " + length + " and width " + width + " is: " + area);
+	    }
+	}
+
